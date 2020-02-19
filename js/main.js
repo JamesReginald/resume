a0d180864d47f7ab4e798b638d3c783b23f21068
JamesReginald <jim@bpool.nl>
2020-02-19 23:07:10 +0100

    footer

--- js/main.js
+++ js/main.js
@@ -8,11 +8,32 @@ $(document).ready(function(){
         $('.project-area .button-group buttons').removeClass('active');
         e.target.classList.add('active');
 
-        let selector = $(e.target).attr("data-filter");
+        let selector = $(e.target).attr('data-filter');
         $('.project-area .grid').isotope({
-            filter: selector
+            filter:selector
         });
 
         return false;
     });
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery:{enabled:true}
+      });
+
+      //owl-carousel
+      $('.site-main .about-area .owl-carousel').owlCarousel({
+          loop: true,
+          autoplay:true,
+          dots: true,
+          responsive:{
+              0:{
+                  items: 1
+              },
+              544:{
+                  items:2
+              }
+          }
+      });
 });
